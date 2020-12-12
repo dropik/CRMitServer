85221e41d89b3cb7600f3563bebfdc7bb1ab6b82
Daniil Ryzhkov <drop.sovet@gmail.com>
2020-12-12 18:11:40 +0200

    Добавьте файлы проекта.

---
+++ CRMitServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRMitServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
