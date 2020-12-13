1d35a6ccfe7a3a86f03614b0805f0ea3f7fbae2f
Daniil Ryzhkov <drop.sovet@gmail.com>
2020-12-13 22:28:37 +0200

    Implemented concrete IEventContainer.

---
+++ CRMitServer/Core/EventContainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CRMitServer.Core
+{
+    public class EventContainer : IEventContainer
+    {
+        public event EventHandler<ClientEventArgs> Purchase;
+
+        public void SendPurchaseMessage(ClientEventArgs args)
+        {
+            Purchase?.Invoke(this, args);
+        }
+    }
+}
---
+++ CRMitServerUnitTests/UnitTests/EventContainer.test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+using CRMitServer.Core;
+
+namespace CRMitServer.UnitTests
+{
+    [TestFixture]
+    public class EventContainerTests
+    {
+        const string CLIENT_NAME = "Ivan";
+
+        [Test]
+        public void TestSendPurchaseMessageInvokesPurchaseEvent()
+        {
+            var eventContainer = new EventContainer();
+            eventContainer.Purchase += AssertEventCalled;
+            var client = new Client()
+            {
+                Name = CLIENT_NAME
+            };
+            var args = new ClientEventArgs(client);
+
+            eventContainer.SendPurchaseMessage(args);
+            Assert.Fail("Event Purchase was not invoked.");
+        }
+
+        private void AssertEventCalled(object sender, ClientEventArgs args)
+        {
+            Assert.That(args.TargetClient.Name == CLIENT_NAME);
+            Assert.Pass();
+        }
+    }
+}
